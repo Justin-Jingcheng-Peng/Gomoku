4acf36594960b155eced460e254d3f163fc0004b
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2022-01-23 15:54:06 -0500

    finished the skeleton of the html and css part, but not styled well

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const container = document.querySelector('#container');
 
 for (let i = 0; i < 9; i++){
     const newImg = document.createElement('img');
-    newImg.src = "x.jpg";
+    newImg.src = "none.jpg";
     newImg.id = 'grid';
     container.appendChild(newImg);
 }
