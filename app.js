b428a5a30faad922da06721051cdd8db8e5b1093
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2022-01-23 23:25:56 -0500

    make some progress in the board

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const container = document.querySelector('#container');
+const total_cells = 9;
+var num_terms = 0;
+board = [];
 
+for (let i = 0; i < 3; i++){
+    board[i] = [];
+    for (let j = 0; j < 3; j++){
+        board[i][j] = 0;
+    }
+}
+
+for (var i = 0; i < 3; i++){
+    for (var j = 0; j < 3; j++){
+        console.log(board[i][j]);
+    }
+}
+
+function handClick(e){
+  
+    if (num_terms % 2 == 0){
+        this.src = "x.jpg";
+    }
+    else{
+        this.src = "o.jpg";
+    }
+    num_terms++;
+    
+}
 for (let i = 0; i < 9; i++){
     const newImg = document.createElement('img');
     newImg.src = "none.jpg";
-    newImg.id = 'grid';
+    newImg.id = `grid ${i}`;
+    newImg.addEventListener('click', handClick, {once:true});
     container.appendChild(newImg);
 }
 
