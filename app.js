d908140e8816080351e2a9aa4ffd4130fc303ac8
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2022-01-24 22:43:10 -0500

    the basic Gomoku board is all set

--- app.js
+++ app.js
@@ -1,45 +1,61 @@
 const container = document.querySelector('#container');
-const total_cells = 9;
+
+const total_cells = 361;
 var num_terms = 0;
 board = [];
-
-for (let i = 0; i < 3; i++){
+// creates a 2D arr with all 0s;
+for (let i = 0; i < 19; i++){
     board[i] = [];
-    for (let j = 0; j < 3; j++){
+    for (let j = 0; j < 19; j++){
         board[i][j] = 0;
     }
 }
 
-for (var i = 0; i < 3; i++){
-    for (var j = 0; j < 3; j++){
-        console.log(board[i][j]);
+function GetStatusImg(img){
+    if (img.src === "none.jpg"){
+        return "none";
     }
-}
-function node(obj, pos_x, pos_y){
-    this.obj = obj;
-    this.pos_x = pos_x;
-    this.pos_y = pos_y;
- 
-}
-function handClick(e){
-  
-    if (num_terms % 2 == 0){
-        this.src = "x.jpg";
-
+    else if (img.src === "x.jpg"){
+        return "x";
     }
     else{
-        this.src = "o.jpg";
+        return "o";
     }
+}
+
+
+function handClick(e){
+    
+    if (num_terms % 2 == 0){
+        // first change the display of this img
+        this.src = "x.jpg";
+        // update the board;
+      
+    }
+    else{
+        // first change the display of this img;
+        this.src = "o.jpg";
+        // update the board;
+ 
+    }
+    
     num_terms++;
     
 }
-for (let i = 0; i < 9; i++){
+//initialize the board;
+// The board contains the nodes;
+// In each node, there is the img object (none.jpg), and the position of the node;
+for (let i = 0; i < 361; i++){
     const newImg = document.createElement('img');
+    // init all the pics with none.jpg
     newImg.src = "none.jpg";
-    newImg.id = `grid ${i}`;
+    // set the eventlistener for all clicks;
     newImg.addEventListener('click', handClick, {once:true});
-    
-    board[Math.floor(i/3)][i%3] = new node(newImg, Math.floor(i/3) + 1, i%3 + 1);
+    // set up the coordinates for all the imgs
+    newImg.setAttribute("x", `${Math.floor(i/19)}`);
+    newImg.setAttribute("y", `${i%19}`);
+    // put all the imgs inside the board;
+    board[Math.floor(i/19)][i%19] = newImg;
     container.appendChild(newImg);
 }
 
